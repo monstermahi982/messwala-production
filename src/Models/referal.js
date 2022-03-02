103595c07dd431d1f735e687365fd84b7a54bf10
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-03-02 21:02:54 +0530

    fixed count to 1

--- src/Models/referal.js
+++ src/Models/referal.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const referalSchema = new Schema({
     name: { type: String, required: true },
     refer_id: { type: String, required: true, unique: true },
-    refer_count: { type: Number, required: false, default: 0 },
+    refer_count: { type: Number, required: false, default: 1 },
     is_active: { type: Boolean, required: false, default: true },
     reward: { type: Boolean, required: false, default: false }
 }, { timestamps: { currentTime: () => Math.floor(Date.now() / 1000) } })
